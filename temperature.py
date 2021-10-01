6cfa5830f716edc6761e6554743834f04044218b
gabrizl <57680354+gabrizl@users.noreply.github.com>
2021-10-01 12:23:47 -0300

    Create temperature.py

---
+++ temperature.py
@@ -0,0 +1,5 @@
+
+print('Passar graus Celsius para Fahrenheit')
+C = float(input('Insira o valor da temperatura em graus Celsius: '))
+temp = (C * 9/5) + 32
+print('\n A temperatura em Fahrenheit é de:', round(temp, 1), 'ºF')
