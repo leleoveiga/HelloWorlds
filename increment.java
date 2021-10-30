e852935171d37922d1af6ad2cac52e0a540bb27e
Paulo Hernane <paulohernane10@gmail.com>
2021-10-30 13:17:44 -0300

    increment.java

---
+++ increment.java
@@ -0,0 +1,14 @@
+class Increment {
+    public static void main(String[] args) {
+        // Todos (anda é longe) os jeitos de incrementar uma variavel:
+        int i = 0;
+        i++;
+        ++i;
+        i = i++;
+        i = ++i;
+        i += 1;
+        i -= -1;
+        i += (int) (Double.MAX_VALUE/Double.MAX_VALUE);
+        //
+    }
+}
